d8ebc4230c086f992c347cd241a5ad5e59a19fbb
Andika Dwiguna <andikadwiguna92@gmail.com>
2026-03-12 14:15:37 +0700

    update perubahan

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,20 +1,19 @@
 import Image from "next/image";
 
-
 export default function Sidebar() {
   return (
-    <div className="p-8">
+    <div className="p-6 md:p-8">
 
-      <div className="text-center mb-10">
-        <Image
-          src="/image/fotoCv.jpg"
-          alt="profile"
-          width={140}
-          height={140}
-          className="rounded-full mx-auto"
-        />
+      <div className="text-center mb-8">
+       <Image
+  src="/image/fotoCv.jpg"
+  alt="profile"
+  width={140}
+  height={140}
+  className="mx-auto rounded-lg object-cover"
+/>
 
-        <h1 className="text-2xl font-bold mt-4">
+        <h1 className="text-xl md:text-2xl font-bold mt-4">
           Andika Dwiguna
         </h1>
 
@@ -23,17 +22,34 @@ export default function Sidebar() {
         </p>
       </div>
 
-      <div className="mb-8">
+      {/* CONTACT */}
+      <div className="mb-8 text-center md:text-left">
         <h2 className="text-lg font-semibold mb-2">Contact</h2>
 
-        <p>Email: andikadwiguna92@Gmail.com</p>
-        <p>Phone: 083825994443</p>
+        <a
+          href="mailto:andikadwiguna92@gmail.com"
+          className="block hover:text-blue-300"
+        >
+          Email: andikadwiguna92@gmail.com
+        </a>
+
+        <a
+          href="https://wa.me/6283825994443"
+          target="_blank"
+          className="block hover:text-green-400"
+        >
+          Phone: 083825994443
+        </a>
+
       </div>
 
+      {/* SKILLS */}
       <div>
-        <h2 className="text-lg font-semibold mb-4">Skills</h2>
+        <h2 className="text-lg font-semibold mb-4 text-center md:text-left">
+          Skills
+        </h2>
 
-        <div className="grid grid-cols-2 gap-2">
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-3 justify-items-center md:justify-items-start">
 
           <div className="skill-circle">HTML</div>
           <div className="skill-circle">CSS</div>
